f6e1b2eeff48d237561dc46aac319861104c6133
tomekgadek <gadektomek@gmail.com>
2025-12-11 13:51:29 +0100

    kata-springboot, dodano zaleznosci spring security

---
+++ src/main/java/io/github/tomekgadek/kata/infrastructure/authentication/AuthenticationConfiguration.java
@@ -0,0 +1,12 @@
+package io.github.tomekgadek.kata.infrastructure.authentication;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class AuthenticationConfiguration {
+    @Bean
+    CurrentUserGetter currentUserGetter() {
+        return new CurrentUserGetter();
+    }
+}
---
+++ src/main/java/io/github/tomekgadek/kata/infrastructure/authentication/CurrentUserGetter.java
@@ -0,0 +1,25 @@
+package io.github.tomekgadek.kata.infrastructure.authentication;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import static org.springframework.util.StringUtils.hasText;
+
+public class CurrentUserGetter {
+
+    private String getSignedInUserName() {
+        String loggedUser = null;
+        SecurityContext context = SecurityContextHolder.getContext();
+        Authentication authentication = context.getAuthentication();
+        if (authentication != null) {
+            loggedUser = authentication.getName();
+        }
+        return loggedUser;
+    }
+
+    public String getSignedInUserNameOrAnonymous() {
+        String signedInUserName = getSignedInUserName();
+        return (hasText(signedInUserName)) ? getSignedInUserName() : "anonymous";
+    }
+}
